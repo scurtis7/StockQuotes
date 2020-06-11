a6419a369efedf99ec9e727d3167bc13f96dc7ae
scurtis7 <scurtis7@gmail.com>
2020-06-11 07:54:08 -0400

    add logger to controller

--- Controllers/StockQuoteController.cs
+++ Controllers/StockQuoteController.cs
@@ -10,16 +10,19 @@ namespace Controllers
     [Route("api/quote")]
     public class StockQuoteController : ControllerBase
     {
+        private readonly ILogger _logger;
         private readonly ClientService _clientService;
 
-        public StockQuoteController(ClientService clientService)
+        public StockQuoteController(ILogger<StockQuoteController> logger, ClientService clientService)
         {
+            _logger = logger;
             _clientService = clientService;
         }
 
         [HttpGet("{symbol}")]
         public async Task<Stock> GetQuote(string symbol)
         {
+            _logger.LogInformation($"GetQuote for symbol: {symbol}");
             var result = await _clientService.GetQuote(symbol);
             return result;
         }
